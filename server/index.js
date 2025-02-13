8da6572f75af2f6abe99c38de1e77dfd762e252b
Joelogical <jmsmith.coding@gmail.com>
2025-02-13 15:09:32 -0800

    fixed package.json

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Handle API routes here
+// app.get('/api/something' ...
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.listen(port, () => {
