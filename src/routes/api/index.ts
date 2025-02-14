d59ce195dc09d3efce62ecdf5faa99486a378afd
Joel S <jmsmith.coding@gmail.com>
2025-02-13 21:33:37 -0800

    worked on src/routes/ files

---
+++ src/routes/api/index.ts
@@ -0,0 +1,4 @@
+// Remove .ts extensions and fix imports
+import "./movieRoutes";
+import "./auth-routes";
+import movieRoutes from "./movieRoutes"; // Changed from { movieRoutes }
---
+++ src/routes/api/rankingRoutes.ts
@@ -0,0 +1,3 @@
+router.get("/", (_req, res) => {
+  // ... rest of the route handler
+});
