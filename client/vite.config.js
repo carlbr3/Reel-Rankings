80376dc8d953cfb4f4817f08c8375ff351785c56
Joelogical <jmsmith.coding@gmail.com>
2025-02-16 20:04:57 -0800

    simplified configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,25 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-// https://vite.dev/config/
-export default defineConfig({
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ command }) => {
+  const config = {
     plugins: [react()],
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/';
+  }
+
+  return {
+    ...config,
     server: {
-        port: 3000,
-        open: true
+      port: process.env.PORT || 3000,
+      host: '0.0.0.0',
+    },
+    preview: {
+      port: process.env.PORT || 3000,
+      host: '0.0.0.0',
     }
+  };
 });
