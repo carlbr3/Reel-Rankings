f9003f859ea9527575d901b9e6a05d87d3fa09eb
Joelogical <jmsmith.coding@gmail.com>
2025-02-16 19:48:12 -0800

    set host: true

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.PORT || '3000'),
-    host: '0.0.0.0'
+    port: parseInt(process.env.PORT || "3000"),
+    host: true,
+    strictPort: true,
   },
   preview: {
-    port: parseInt(process.env.PORT || '3000'),
-    host: '0.0.0.0',
-    allowedHosts: ['reel-rankings.onrender.com']
-  }
-}) 
+    port: parseInt(process.env.PORT || "3000"),
+    host: true,
+    strictPort: true,
+  },
+});
