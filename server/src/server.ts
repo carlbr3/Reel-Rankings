d8c0b93570d38c2bb5dd9557a9293c7734229e68
Joel S <jmsmith.coding@gmail.com>
2025-02-17 20:27:12 -0800

    server added?

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
-import path from 'path';
+import path from "path";
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back the index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-}); 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+});
